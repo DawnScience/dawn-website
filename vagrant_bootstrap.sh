4c8f9ead5f0c0d406daee7aeb19221a3b991699a
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-09-10 13:46:45 +0100

    Fix vagrant bootstrap script

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -37,13 +37,17 @@ sudo apt-get install -y ${apt_packages[@]}
 sudo apt-get clean
 
 # http://rvm.io/rvm/install
-#gpg --keyserver hkp://keys.gnupg.net:80 --recv-keys D39DC0E3
-#gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://rvm.io/mpapis.asc | gpg --import
+for server in ha.pool.sks-keyservers.net \
+              hkp://p80.pool.sks-keyservers.net:80 \
+              keyserver.ubuntu.com \
+              hkp://keyserver.ubuntu.com:80 \
+              pgp.mit.edu; do
+    gpg --keyserver "$server" --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB && break || echo "Trying new server..."
+done
 curl -sSL https://get.rvm.io | bash -s stable --quiet-curl
 source ~/.rvm/scripts/rvm
-rvm install 2.1.7 --quiet-curl
-rvm use 2.1.7 --default
+rvm install 2.6.4 --quiet-curl
+rvm use 2.6.4 --default
 ruby --version
 
 # https://github.com/github/pages-gem
